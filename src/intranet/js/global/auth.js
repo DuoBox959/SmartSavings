1d8de5aec3a3955099c738db8f265561671ae3e7
rferpae569 <rferpae569@g.educaand.es>
2025-03-19 22:46:53 +0100

    19/03/2025/22:46

---
+++ src/intranet/js/global/auth.js
@@ -0,0 +1,13 @@
+// Validar si el usuario está autenticado
+function verificarAutenticacion() {
+    const user = sessionStorage.getItem("user");
+  
+    if (!user) {
+      alert("⚠️ Debes iniciar sesión para acceder a esta página.");
+      window.location.href = "../html/login.html"; // Redirige al login
+    }
+  }
+  
+  // Ejecutar la verificación al cargar la página
+  document.addEventListener("DOMContentLoaded", verificarAutenticacion);
+  
