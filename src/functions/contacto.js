6f0910a7a29fbc1d90edcfb40c8007f615411a6f
rferpae569 <rferpae569@g.educaand.es>
2025-04-17 12:34:25 +0200

    17/04/2025/12:34

---
+++ src/functions/contacto.js
@@ -0,0 +1,34 @@
+import { volverAtras } from "../functions/global/funciones.js";
+
+// Hacer el botón de volver atrás funcional
+window.volverAtras = volverAtras;
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // 👇 Inicializar con clave publica
+  emailjs.init(EMAILJS_PUBLIC_KEY);
+
+  const form = document.querySelector('form');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(EMAILJS_SERVICE_ID, EMAILJS_TEMPLATE_ID, form)
+      .then(() => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Mensaje enviado',
+          text: 'Gracias por escribirnos. Te responderemos pronto.',
+        });
+        form.reset();
+      })
+      .catch((error) => {
+        console.error('Error al enviar el mensaje:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'No se pudo enviar el mensaje. Intenta nuevamente más tarde.',
+        });
+      });
+  });
+});
