3d89a7483e06af395c395c8ce6f94e909aab1e47
DuoBox959 <duobox959@gmail.com>
2025-04-27 17:24:18 +0200

    27/04/25/17:24

--- server/STATIC/estatico.js
+++ server/STATIC/estatico.js
@@ -1,9 +1,8 @@
-// server/STATIC/estatico.js
 const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-// 📌 Servir archivos estáticos correctamente
-router.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
+// 📦 Servir carpeta de imágenes
+router.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 module.exports = router;
